536e80e7c7c90fd6fbce4a844eaefc3203f2b0b5
JulioCoi <amorim.jc@gmail.com>
2025-03-03 01:56:35 -0300

    feat: conexão com banco de dados

---
+++ src/config/db.ts
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: './src/config/.env' });
+
+const DB = process.env.DATABASE!.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD!,
+);
+
+console.log(DB);
+export const configDB = async () => {
+  try {
+    await mongoose.connect(DB);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
